bf3252c837d35ff06b3da65c85e1f3ce82bf8a10
david <davyd_d4v1d@hotmail.com>
2019-08-24 02:34:18 -0500

    Serializers de pagos alunos y materias asignada se agrego un readonly

--- MateriasAsignadas/serializers.py
+++ MateriasAsignadas/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import routers, serializers, viewsets
 from MateriasAsignadas.models import MateriasAsignadas
 
 class MateriasAsignadasSerializers(serializers.ModelSerializer):
+    nombre_maestro = serializers.ReadOnlyField(source='maestro.nombre')
+    nombre_materia = serializers.ReadOnlyField(source='materia.nombre')
     class Meta:
         model = MateriasAsignadas
         fields = ('__all__')
--- PagoAlumnos/serializers.py
+++ PagoAlumnos/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import routers, serializers, viewsets
 from PagoAlumnos.models import PagoAlumnos
 
 class PagoAlumnosSerializers(serializers.ModelSerializer):
+    nombre_alumno = serializers.ReadOnlyField(source='alumno.nombre')
+    apellidoP_alumno = serializers.ReadOnlyField(source='alumno.apellidopaterno')
+    apellidoA_alumno = serializers.ReadOnlyField(source='alumno.apellidomaterno')
     class Meta:
         model = PagoAlumnos
         fields = ('__all__')
